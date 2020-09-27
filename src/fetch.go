452bed16a1ad1a0f5721799341235630a11e9211
Eddie901 <eddie901@gmail.com>
2020-09-27 13:42:28 +0100

    Modified fetch to add prefix if missing and report status code

--- src/fetch.go
+++ src/fetch.go
@@ -6,10 +6,14 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
@@ -21,6 +25,8 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
+		fmt.Printf("Reading from %s\n", url)
+		fmt.Printf("HTTP Status: %s\n", resp.Status)
 		fmt.Printf("%s", b)
 	}
 }
