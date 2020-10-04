4bf10fa0d4f58ca2ef7fccc181a16389918a022f
Eddie901 <eddie901@gmail.com>
2020-10-04 12:18:00 +0100

    Ch2: tempconv package

--- src/ftoc.go
+++ src/ftoc.go
@@ -1,14 +1,18 @@
 // Ftoc print two Fahrenheit-to-Celsius conversions
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"tempconv"
+)
 
 func main(){
-	const freezingF, boilingF = 32.0, 212.0
-	fmt.Printf("%gºF = %gºC\n", freezingF, fToC(freezingF))
-	fmt.Printf("%gºF = %gºC\n", boilingF, fToC(boilingF))
-}
-
-func fToC(f float64) float64 {
-	return (f - 32) * 5 / 9
+	fmt.Printf("Brrr! %v\n", tempconv.AbsoluteZeroC)
+	fmt.Printf("[In Kelvin] Brrr! %v\n", tempconv.CToK(tempconv.AbsoluteZeroC))
+	fmt.Printf("Freezing point [In Kelvin] Brrr! %v\n", tempconv.CToK(tempconv.FreezingC))
+	fmt.Printf("Freezing point [In Celsius] Brrr! %v\n", tempconv.FreezingC)
+	fmt.Printf("Freezing point [In Fahrenheit] Brrr! %v\n", tempconv.CToF(tempconv.FreezingC))
+	fmt.Printf("Boiling point [In Celsius] Toastie! %v\n", tempconv.BoilingC)
+	fmt.Printf("Boiling point [In Fahrenheit] Toastie! %v\n", tempconv.CToF(tempconv.BoilingC))
+	fmt.Printf("Boiling point [In Kelvin] Toastie! %v\n", tempconv.CToK(tempconv.BoilingC))
 }
