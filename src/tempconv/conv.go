84b52902ff47b367874e6803f6cb451cceda0849
Eddie901 <eddie901@gmail.com>
2020-10-04 12:28:10 +0100

    Ch2: cf temp conversion using cmd line args

---
+++ src/tempconv/conv.go
@@ -0,0 +1,6 @@
+package tempconv
+
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+func KToC(k Kelvin) Celsius { return Celsius(k + Kelvin(AbsoluteZeroC)) }
+func CToK(c Celsius) Kelvin { return Kelvin(c - AbsoluteZeroC) }
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
